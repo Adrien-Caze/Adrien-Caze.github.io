8da97824777f57316ff6acd0a16afe8ea6a2d638
Adrien <adrien.caze77@gmail.com>
2026-05-29 11:28:38 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,7 @@ function openDoom() {
       wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
     }).ready((fs, main) => {
       fs.extract("assets/doom.zip").then(() => {
-        main(["-c", "DOOM.EXE"]);
+        main(["-c", "doom.EXE"]);
     });
     });
   }
