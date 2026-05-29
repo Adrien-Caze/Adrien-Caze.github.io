dfdcb709b94acf90f708a151f6c58cf416edd3d3
Adrien <adrien.caze77@gmail.com>
2026-05-29 11:27:02 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,14 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+
+// --- Easter Egg DOOM : taper "1993" sur n'importe quelle page ---
+
 let typedSequence = '';
 const SECRET_CODE = '1993';
 let doomLoaded = false;
@@ -40,9 +48,9 @@ function openDoom() {
     Dos(document.getElementById('doomCanvas'), {
       wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
     }).ready((fs, main) => {
-      fs.extract("https://js-dos.com/6.22/games/doom.zip").then(() => {
+      fs.extract("assets/doom.zip").then(() => {
         main(["-c", "DOOM.EXE"]);
-      });
+    });
     });
   }
 }
